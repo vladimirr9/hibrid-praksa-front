29c0901d897df021b1ff776155758007a19d3a4a
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2021-10-05 13:18:35 +0200

    Initial toolbar made

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-
 import { BooksService } from '../books.service';
 import { BookCardComponent } from '../book-card/book-card.component';
 
