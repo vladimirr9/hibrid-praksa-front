f87689fa526c3623d4d9be8dc1a948f4ed5f8430
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2021-10-07 15:28:58 +0200

    Added logo path to config

--- src/app/navbar/navbar/navbar.component.ts
+++ src/app/navbar/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import config from './../../shared.json';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+
+  public logo = config.logo;
+  
+
 
   constructor() { }
 
