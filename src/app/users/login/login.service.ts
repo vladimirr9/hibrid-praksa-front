51b7c6cb95d5c7808da2b70b6d7d0e0b86124a5f
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2021-10-12 10:13:55 +0200

    Moved local storage access to loginService and set role based on jwt in local storage

--- src/app/users/login/login.service.ts
+++ src/app/users/login/login.service.ts
@@ -1,12 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import config from "./../../shared.json"
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
+  
+
   public isLoggedIn(): boolean {
     return  localStorage.getItem('username') != null
 }
-  constructor() { }
+  
+
+  public getUsername() : string | null{
+    return localStorage.getItem('username')
+  }
+  public getToken() {
+    return localStorage.getItem('token')
+  }
+
+  constructor(private http: HttpClient) { }
 }
