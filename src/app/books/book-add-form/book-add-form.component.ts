7497cef6fd5cb32959fc8a0d82c3f78daa8e1a12
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2021-10-13 12:39:32 +0200

    Fixed form validation

--- src/app/books/book-add-form/book-add-form.component.ts
+++ src/app/books/book-add-form/book-add-form.component.ts
@@ -3,6 +3,7 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { DateAdapter } from '@angular/material/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { Author } from 'src/app/authors/author';
 import { AuthorDialogComponent } from 'src/app/authors/author-dialog/author-dialog.component';
 import { AuthorService } from 'src/app/authors/author.service';
@@ -18,23 +19,29 @@ export class BookAddFormComponent implements OnInit {
   constructor(private fb: FormBuilder,
               private authorService: AuthorService,
               private bookService: BooksService,
-              private dialog: MatDialog) { }
+              private dialog: MatDialog,
+              private router: Router) { }
 
   authors : Author[] = []
 
+  isbnReg: string = "^(?=(?:\\D*\\d){10}(?:(?:\\D*\\d){3})?$)[\\d-]+$";
+
   selectedAuthors : Author[] = []
 
+  public submitFailed : boolean = false
+
   addBookForm = this.fb.group({
-    title: [''],
-    description: [''],
+    title: ['', Validators.required],
+    description: '',
     authors: this.fb.array([]),
-    creationDate: [''],
-    isbn: [''],
+    creationDate: ['', Validators.required],
+    isbn: ['', Validators.required],
     quantity: [''],
     imageUrl: ['']
   })
   
   ngOnInit(): void {
+
     this.authorService.getAuthors().subscribe(data => {
       this.authors = data.map( item => {
         return {
@@ -72,8 +79,10 @@ export class BookAddFormComponent implements OnInit {
 
   onSubmit() {
     if (this.addBookForm.invalid) {
+      this.submitFailed = true
       return;
     }
+    this.submitFailed = false
     this.bookService.postBook( {
     title: this.addBookForm.get('title')?.value,
     description: this.addBookForm.get('description')?.value,
@@ -83,7 +92,9 @@ export class BookAddFormComponent implements OnInit {
     quantity: this.addBookForm.get('quantity')?.value,
     imageUrl: this.addBookForm.get('imageUrl')?.value
     }).subscribe( data => {
-      console.log(data)
+      this.router.navigateByUrl('/books')
+    }, (errorResponse: any) => {
+      this.submitFailed = true
     })
 
   }
